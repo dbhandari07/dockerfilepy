c70b77b270d80e58cfa19bb918994375a8bf3271
dbhandari07 <75921797+dbhandari07@users.noreply.github.com>
2023-04-12 18:19:34 +0545

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def make_app():
         (r"/", MainHandler),
         ])
 
-if __name__ ++ "__main__":
+if __name__ == "__main__":
    app = make_app()
    app.listen(8888)
    tornado.ioloop.IOLoop.current().start()

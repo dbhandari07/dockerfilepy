e4e6e84c1dd50657fb2b342217fcdb19fec611b6
root <root@k8s.local>
2023-04-19 17:36:50 +0545

    added File

---
+++ server1.py
@@ -0,0 +1,20 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = ("Hello, Dipendra\n")
+        bgcolor = "limegreen"
+        self.render("template.html", title=title, bgcolor=bgcolor)
+        print(self.request)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        ])
+
+if __name__ == "__main__":
+   app = make_app()
+   app.listen(8888)
+   tornado.ioloop.IOLoop.current().start()
+
